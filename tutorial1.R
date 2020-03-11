08d9f45bac25c9cdd94565a88bcba3ecf9aba0ad
techtribeyt <46093510+techtribeyt@users.noreply.github.com>
2020-03-11 13:24:26 -0500

    Add files via upload

---
+++ tutorial1.R
@@ -0,0 +1,18 @@
+# this is a comment
+a <- 2
+b <- 3
+
+lst = c(1, 2, 3)
+
+c <- a + b
+
+rand1 <- rnorm(100)
+rand2 <- rnorm(100)
+
+draws <- cbind(rand1, rand2)
+plot(draws)
+
+d <- c(1:100)
+
+draws2 <- cbind(d, d)
+plot(draws2)
